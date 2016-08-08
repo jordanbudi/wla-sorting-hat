6ade85b79baad9a790b65e9bc3471e61b91df1a4
Jordan Budisantoso <jordanbudi@gmail.com>
2016-08-08 21:07:16 +0000

    HP and GoT examples are loaded in

--- js/script.js
+++ js/script.js
@@ -1,40 +1,85 @@
 $(document).ready(function(){
-    $("#orb").click(function(){
-        if($(this).hasClass("sun")){
-          $(this).removeClass("sun").addClass("moon");
-        }
-        else{
-          $(this).removeClass("moon").addClass("sun");
-        }
-        
-        if($("#sky").hasClass("day")){
-          $("#sky").removeClass("day").addClass("night");
-        }
-        else{
-          $("#sky").removeClass("night").addClass("day");
-        }
-        
-        if($("#moonspot1").hasClass("visible")){
-          $("#moonspot1").removeClass("visible");
-        } 
-        else{
-          $("#moonspot1").addClass("visible");
-        }
-      
-        if($("#moonspot2").hasClass("visible")){
-          $("#moonspot2").removeClass("visible");
-        } 
-        else{
-          $("#moonspot2").addClass("visible");
-        }
-      
-        if($("#moonspot3").hasClass("visible")){
-          $("#moonspot3").removeClass("visible");
-        } 
-        else{
-          $("#moonspot3").addClass("visible");
-        }
-      
-    });
-});
 
+  function changeState(color){
+    if($("#orb").hasClass("grey-inside")){
+      $("#orb").removeClass("grey-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("red-inside")){
+      $("#orb").removeClass("red-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("orange-inside")){
+      $("#orb").removeClass("orange-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("yellow-inside")){
+      $("#orb").removeClass("yellow-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("green-inside")){
+      $("#orb").removeClass("green-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("blue-inside")){
+      $("#orb").removeClass("blue-inside").addClass(color+"-inside");
+    }
+    else if($("#orb").hasClass("purple-inside")){
+      $("#orb").removeClass("purple-inside").addClass(color+"-inside");
+    }
+
+    if($("#background").hasClass("grey-background")){
+      $("#background").removeClass("grey-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("red-background")){
+      $("#background").removeClass("red-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("orange-background")){
+      $("#background").removeClass("orange-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("yellow-background")){
+      $("#background").removeClass("yellow-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("green-background")){
+      $("#background").removeClass("green-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("blue-background")){
+      $("#background").removeClass("blue-background").addClass(color+"-background");
+    }
+    else if($("#background").hasClass("purple-background")){
+      $("#background").removeClass("purple-background").addClass(color+"-background");
+    }
+  };
+
+  $(window).keypress(function(letter) {
+    var key = letter.keyCode
+    if (key === 97 || key === 65)
+    {
+      changeState("red");
+    }
+    else if (key === 115 || key === 83)
+    {
+      changeState("orange");
+    }
+    else if (key === 100 || key === 68)
+    {
+      changeState("yellow");
+    }
+    else if (key === 102 || key === 70)
+    {
+      changeState("green");
+    }
+    else if (key === 103 || key === 71)
+    {
+      changeState("blue");
+    }
+    else if (key === 104 || key === 72)
+    {
+      changeState("purple");
+    }
+    else if (key === 48)
+    {
+      changeState("grey");
+    }
+    // else if (key === 106 || key === 74)
+    // {
+    //   changeState("pink");
+    // }
+  });
+
+});
